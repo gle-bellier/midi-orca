1aa5ee24ed272c3e7b6510e64d0feec54b5e1a6c
georges lebellier <georges.lebeller@gmail.com>
2022-06-06 23:57:41 +0200

    Refactor code

--- src/midi_reader.py
+++ src/midi_reader.py
@@ -1,48 +1,6 @@
-from dataclasses import dataclass
 from typing import List
 import pretty_midi
 import numpy as np
-
-
-@dataclass
-class Note:
-    pitch: int
-    start: int
-    end: int
-    velocity: int
-
-    @property
-    def octave(self) -> int:
-        return self.pitch // 12
-
-    @property
-    def chroma(self) -> int:
-        return self.pitch % 12
-
-    @property
-    def duration(self) -> int:
-        return self.end - self.start
-
-    def encode_midi(self, n: int) -> str:
-        n = int((n / 128) * 36)
-        return self.encode36(n)
-
-    def encode36(self, n: int) -> str:
-        return np.base_repr(n, base=36)
-
-    def encode_chroma(self, n: int) -> str:
-        return list("CcDdEFfGgAaB")[n]
-
-    @property
-    def orca(self) -> str:
-        if self.velocity == 0:
-            return "...."
-        else:
-            octave = str(self.octave)
-            chroma = self.encode_chroma(self.chroma)
-            vel = self.encode_midi(self.velocity)
-            duration = self.encode36(int(self.duration))
-            return octave + chroma + vel + duration
 
 
 class MidiReader:
@@ -63,29 +21,27 @@ class MidiReader:
         # quantize
         q_notes = []
         for n in notes:
-            s = int(n.start / time_step)
-            e = int(n.end / time_step)
-            q_notes += [Note(n.pitch, s, e, n.velocity)]
+            s = np.round(n.start / time_step)
+            e = np.round(n.end / time_step)
+            q_notes += [Note(n.pitch, int(s), int(e), n.velocity)]
 
         return q_notes
 
     def get_mono(self, path: str) -> List[np.ndarray]:
 
         notes = self.get_notes(path)
-        length = notes[-1].start
-        length = length + (length - length % self.pattern_length)
+        length = notes[-1].start + 1
+        length += (self.pattern_length - length % self.pattern_length) * (
+            length % self.pattern_length != 0)
         patterns = [Note(0, i, i + 1, 0) for i in range(length)]
 
         for note in notes:
             patterns[note.start] = note
 
+        patterns = np.array(patterns)
+        patterns = patterns.reshape((-1, self.pattern_length))
+
         print(patterns)
-        for note in notes:
-            print(note)
-        print("_____________________________")
-        print("_____________________________")
-        print("_____________________________")
-        print([n.orca for n in patterns])
 
 
 if __name__ == '__main__':
